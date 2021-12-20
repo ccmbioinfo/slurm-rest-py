6558efb6a3f601664d64ce871c9d0038668105d2
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2021-12-20 15:21:18 -0500

    0.0.37.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "slurm-rest"
-VERSION = "0.0.37.0"
+VERSION = "0.0.37.1"
 # To install the library, run the following
 #
 # python setup.py install
--- slurm_rest/__init__.py
+++ slurm_rest/__init__.py
@@ -11,7 +11,7 @@
 """
 
 
-__version__ = "0.0.37.0"
+__version__ = "0.0.37.1"
 
 # import ApiClient
 from slurm_rest.api_client import ApiClient
